c95ff50423a92f4e3efce29b4125bf231a77ea71
AlejandroMtz07 <alejandro23juanito@gmail.com>
2024-02-27 21:02:23 -0700

    Se agrega el proyecto

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'Tarjeta de valores';
+  description = 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500';
+  card = {
+    title:'Tarjeta de valores',
+    description:'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500',
+    aviso:'Debes de ingresar datos',
+    contenido:'Este es un modal lanzado por un boton'
+  }
+  
+}
